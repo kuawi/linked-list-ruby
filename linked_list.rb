acdf2d7060fcdba1e855c349df6b0baa563e8da8
Jose L. Lozano <jllozano.mx@gmail.com>
2021-04-16 13:53:29 -0500

    Add append and to_s methods

--- linked_list.rb
+++ linked_list.rb
@@ -8,13 +8,41 @@ class Node
 end
 
 class LinkedList
-  attr_accessor :head
+  attr_accessor :head, :tail, :size
 
   def initialize
-    @head = nil
+    @head = @tail = nil
+    @size = 0
   end
 
   def prepend(value = nil)
+    @size += 1
     @head = Node.new(value, @head)
+    @tail = @head if @size == 1
+    @head.value
+  end
+
+  def append(value = nil)
+    @size += 1
+    temp = Node.new(value)
+    if @tail
+      @tail = @tail.next_node = temp
+    else
+      @head = @tail = temp
+    end
+    @tail.value
+  end
+
+  def to_s
+    next_node = @head
+    string = ''
+    string = 'Empty list' unless next_node
+    while next_node
+      current_node = next_node
+      string += "(#{current_node.value}) -> "
+      next_node = current_node.next_node
+      string += 'nil' unless next_node
+    end
+    string
   end
 end
